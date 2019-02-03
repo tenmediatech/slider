7f028936763c959c469a5054a1532600f64b15f9
Tenzin Migmar <lhasaboy2010@gmail.com>
2019-02-03 16:52:40 -0500

    Made changes to file structure

--- js/index.js
+++ js/index.js
@@ -5,16 +5,16 @@ jQuery(document).ready(function ($) {
         moveRight();
     }, 3000);
   });
-
+  
 	var slideCount = $('#slider ul li').length;
 	var slideWidth = $('#slider ul li').width();
 	var slideHeight = $('#slider ul li').height();
 	var sliderUlWidth = slideCount * slideWidth;
-
+	
 	$('#slider').css({ width: slideWidth, height: slideHeight });
-
+	
 	$('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-
+	
     $('#slider ul li:last-child').prependTo('#slider ul');
 
     function moveLeft() {
